0d5dbd0d5db804f9b8277240e6dc616fdf2d80c0
nabeehdaouk <144377506+nabeehdaouk@users.noreply.github.com>
2023-09-19 10:30:12 -0400

    updated PC to 11 bits

--- Pipelined_CPU/pc.v
+++ Pipelined_CPU/pc.v
@@ -2,19 +2,19 @@ module pc(
     input clk,
     input reset,
     input branch_valid,
-    input [3:0] branch_address,
-    output reg [3:0] cnt
+    input [10:0] branch_address,
+    output reg [10:0] cnt
 );
 
     always@(posedge clk)
     begin
         if (reset)
-            cnt<= 3'b000;
+            cnt<= 10'b0000000000;
         else
             if (branch_valid)
                 cnt<= branch_address;
             else
-                cnt<= cnt+1;
+                cnt<= cnt+1'b1;
     end
 
 endmodule
