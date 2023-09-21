28f5aae921d08b398d73d1d4d4ae3229f98bc763
Sam Rojanasakdakul <sxr910@case.edu>
2023-09-21 10:29:55 -0400

    Added comments to PC

--- Pipelined_CPU/pc.v
+++ Pipelined_CPU/pc.v
@@ -1,6 +1,6 @@
 module pc(
     input clk, //cpu clock
-    input reset, //reset pc register
+    input resetn, //reset pc register
     input branch_valid, //jump control 
     input [10:0] branch_address, //jump address
     output reg [10:0] cnt //holds program count
@@ -8,12 +8,12 @@ module pc(
 
     always@(posedge clk) 
     begin: program_counter
-        if (reset)
+        if (!resetn)    //When reset is active low count is set to 0
             cnt<= 10'b0000000000;
         else
-            if (branch_valid)
+            if (branch_valid)   //If jump control is active high, count is set to branch address
                 cnt<= branch_address;
-            else
+            else                // Else count = count + 1
                 cnt<= cnt+1'b1;
     end
 
