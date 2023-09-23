7c29661ecdae854ea3cd1a977ba29f7a70f99c8e
Sam Rojanasakdakul <sxr910@case.edu>
2023-09-22 20:32:50 -0400

    Memory additions
    Added read enable, read address, data out. Added read enable block.

--- Pipelined_CPU/memory.v
+++ Pipelined_CPU/memory.v
@@ -2,11 +2,14 @@ module memory(
     input clk,                  //Clock
     input resetn,               //Active low reset
     input [10:0] w_adrs,        //Write address
-    input [10:0] r_adrs,        //Read address
+    input [10:0] r_adrs1,       //Read address 1
+    input [10:0] r_adrs2,       //Read address 2
     input [31:0] data_in,       //Data in
     input w_en,                 //Write enable
-    input r_en,                 //Read enable
-    output reg [31:0] data_out  //Data out
+    input r_en1,                //Read enable 1
+    input r_en2,                //Read enable 2
+    output reg [31:0] data_out1,  //Data out 1
+    output reg [31:0] data_out2   //Data out 2
 );
 
     reg [31:0] mem [2047:0];
@@ -15,7 +18,8 @@ module memory(
     always @(posedge clk) begin: Read_Write_Memory
 
         if (!resetn) begin
-            data_out <= 0;
+            data_out1 <= 0;
+            data_out2 <= 0;
             for(i = 0; i < 2048; i = i + 1) begin
                mem[i] <= 0; 
             end
@@ -23,8 +27,11 @@ module memory(
             if(w_en) begin
                 mem[w_adrs] <= data_in;
             end
-            if(r_en) begin
-               data_out <= mem[r_adrs];
+            if(r_en1) begin
+               data_out1 <= mem[r_adrs1];
+            end
+            if(r_en2) begin
+               data_out2 <=  mem[r_adrs2];
             end
         end    
     end
