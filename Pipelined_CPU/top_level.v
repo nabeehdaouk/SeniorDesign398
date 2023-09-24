a25ee3db105b53b2b43d5624d4a4cf9ed195f520
Jacob Salmon <jsalmon4302@gmail.com>
2023-09-24 17:06:51 -0400

    Reset top-level

--- Pipelined_CPU/top_level.v
+++ Pipelined_CPU/top_level.v
@@ -3,27 +3,4 @@ module top_level(
     input resetn        //Global reset
 );
     
-    cpu cpu_instance(
-        .clk(clk),
-        .resetn(resetn),
-        .instruction_fetch(instruction_fetch),
-        .read_mem(read_mem),
-        .write_mem(write_mem),
-        .carry(carry),
-        .result(result),
-        .mem_wadrs(mem_wadrs),
-        .mem_radrs(mem_radrs)
-    );
-    
-    memory memory_instance(
-        .clk(clk),
-        .resetn(resetn),
-        .w_adrs(w_adrs),
-        .r_adrs(r_adrs),
-        .data_in(data_in),
-        .w_en(w_en),
-        .r_en(r_en),
-        .data_out(data_out)
-    );
-    
 endmodule
