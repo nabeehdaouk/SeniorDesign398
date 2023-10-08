abf64a4625d64065d2ce301713ccf2f503239dd9
Sam Rojanasakdakul <sxr910@case.edu>
2023-10-08 17:47:25 -0400

    Parameterized ff_synch and top level
    parametrized

--- Pipelined_CPU/Async_FIFO/async_fifo_top_level.v
+++ Pipelined_CPU/Async_FIFO/async_fifo_top_level.v
@@ -1,6 +1,12 @@
-module async_fifo_top_level(
-	input clk,
-	input rst
+module async_fifo_top_level #(data_width = 31, mem_depth=8)(
+	input w_clk,
+	input r_clk,
+	input resetn,
+	input [data_width-1:0] w_data,
+	input [mem_depth-1:0] r_adrs,
+	input [mem_depth-1:0] w_adrs,
+	output [data_width:0] r_data
+	
 );
 	
 endmodule
--- Pipelined_CPU/Async_FIFO/ff_sync.v
+++ Pipelined_CPU/Async_FIFO/ff_sync.v
@@ -1,10 +1,10 @@
-module ff_sync(                        //Double flip flop synchronizer
+module ff_sync #(parameter data_width =32)(                        //Double flip flop synchronizer
 	input clk,                         //Global clock signal
 	input resetn,                      //Global reset signal
-	input [31:0] data_in,              //Data in
-	output reg [31:0] data_out         //Data out
+	input [data_width:0] data_in,              //Data in
+	output reg [data_width:0] data_out         //Data out
 );
-	reg [31:0] q1;                     //Output of first flipflop holding data_in
+	reg [data_width:0] q1;                     //Output of first flipflop holding data_in
 	
 	always @(posedge clk) begin
 	    if (!resetn) begin             //If reset set both flip flop outputs to 0
