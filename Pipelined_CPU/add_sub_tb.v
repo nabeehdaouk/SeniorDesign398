5f62f26b1f310cf75053401aeaffc8a1afbbfb16
Jacob Salmon <jsalmon4302@gmail.com>
2023-10-08 17:45:25 -0400

    Testbenches Final

--- Pipelined_CPU/add_sub_tb.v
+++ Pipelined_CPU/add_sub_tb.v
@@ -29,55 +29,43 @@ module add_sub_tb();
         cpu_en = 1;
         w_instruction = 32'h0000_0000;
         w_enable = 0;
-        w_adrs = 1;
         
         //Load the number 10 from memory into reg_a_3
         #10
         cpu_en = 0;
         resetn = 1;
         w_enable = 1;
-        w_adrs = 2;
+        w_adrs = 1;
         w_instruction = 32'b111_00_000_0_0_0_00000_00011_0_00000_01010; //LOAD MEM10 REG_A_3
         
         //Load the number 11 from memory into reg_b_3 
         #10
-        w_adrs = 3;
+        w_adrs = 2;
         w_instruction = 32'b111_00_000_0_0_1_00011_00000_0_00000_01011; //LOAD MEM11 REG_B_3
         
         //Add immediate to value in register a
         #10
-        w_adrs = 4;
-        w_instruction = 32'b100_00_000_1_0_0_00000_00011_0_00000_00001;  //Add 1 REG_A_3
+        w_adrs = 5;
+        w_instruction = 32'b100_00_000_1_0_0_00000_00011_0_00000_00101;  //ADD 5 REG_A_3
       
         //Add immediate to value in register b
         #10
-        w_adrs = 5;
-        w_instruction = 32'b100_00_000_1_0_1_00011_00000_0_00000_00001;  //Add 1 REG_B_3
+        w_adrs = 6;
+        w_instruction = 32'b100_00_000_1_0_1_00011_00000_0_00000_00001;  //ADD 1 REG_B_3
         
+        //Add instruction  
+        #10
+        w_adrs = 7;
+        w_instruction = 32'b011_00_000_1_0_0_00000_00011_0_00000_00001;  //SUB 1 REG_A_3
+
         //Add instruction
         #10
+        w_adrs = 8;
+        w_instruction = 32'b101_00_100_0_0_0_00000_00110_0_00000_00000; //BRA NEZ MEM6
+        
+        #10
         w_adrs = 9;
-        w_instruction = 32'b011_00_000_1_0_0_00000_00011_0_00000_00001;  //Sub 1 REG_A_3
-
-        
-        //Add immediate to register b
-        //Add immediate to memory
-        //Add register a to register a
-        //Add register a to register b
-        //Add register b to register a
-        //Add register b to register b
-        //Add register a to memory
-        //Add register b to memory
-        //Add memory to register a
-        //Add memory to register b 
-        //Add memory to memory
-        
-        //Sub register from immediate
-        //Sub memory from immediate
-        //Sub register from register
-        //Sub memory from register
-        //Sub register from memory
-        //Sub memory from memory
+        w_instruction = 32'b100_00_00_0_0_0_00000_00011_1_00000_00011; //Add REG_B_3 REG_A_3
         
         //Number 10 in memory 10
         #10
--- Pipelined_CPU/and_or_tb.v
+++ Pipelined_CPU/and_or_tb.v
@@ -1,4 +1,4 @@
-module top_level_and_or_tb();
+module and_or_tb();
     
     reg clk;
     reg resetn;        //Global reset
@@ -30,51 +30,43 @@ module top_level_and_or_tb();
         cpu_en = 1;
         w_instruction = 32'h0000_0000;
         w_enable = 0;
-        w_adrs = 1;
         
         //Add instrction
         #10
-         w_adrs = 1;
-        cpu_en = 0;
+        w_adrs = 1;
         resetn = 1;
+        cpu_en = 0;
         w_enable = 1;
         w_instruction = 32'b111_00_000_0_0_0_00000_00011_0_00000_01111; //LOAD MEM15 REG_A_3
         
         //Add instrction
         #10
-         w_adrs = 4;
-        cpu_en = 0;
-        resetn = 1;
-        w_enable = 1;
-          w_instruction = 32'b111_00_000_0_0_1_00101_00101_0_00000_10001; //LOAD MEM16 REG_B_5
-    
+        w_adrs = 2;
+        w_instruction = 32'b111_00_000_0_0_0_00101_00101_0_00000_10001; //LOAD MEM16 REG_A_7
         
         //Add instrction
         #10
-        w_adrs = 12;
-        cpu_en = 0;
-        resetn = 1;
-        w_enable = 1;
+        w_adrs = 7;
+        w_instruction = 32'b001_00_000_0_0_0_00000_00011_0_00000_00101; //OR REGA_3 REGB_7
         
-        
-        //Select AND or OR
-        
-          //w_instruction = 32'b010_00_000_0_0_0_00000_00011_1_00101_00101; //AND REGA_3 REGA_7
-        w_instruction = 32'b001_00_000_0_0_0_00000_00011_1_00101_00101; //OR REGA_3 REGB_7
-        
-        
+        //Add instruction
+        #10
+        w_adrs = 8;
+        w_instruction = 32'b010_00_000_0_0_0_00000_00011_0_00000_00101; //AND REGA_3 REGA_7
+         
+        //Add instruction
+        #10
+        w_adrs = 9;
+        w_instruction = 32'b001_00_000_1_0_1_11111_00000_0_01010_10101; //AND REGB_31 341
         
         //Add values in mem
         #10
         w_adrs = 15;
         w_instruction = 32'hFFFF_0000;
+        
         #10
         w_adrs = 17;
-        w_instruction = 32'hAAAA_AAAA;
-        
-        
-        
-        
+        w_instruction = 32'hAAAA_AAAA;        
         
         //Start
         #10
