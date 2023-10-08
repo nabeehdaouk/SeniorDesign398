59d762235a67f14a60f565e11663896c95eeeae6
Sam Rojanasakdakul <sxr910@case.edu>
2023-10-08 17:39:53 -0400

    Update ram.v
    Parameterized RAM

--- Pipelined_CPU/Async_FIFO/ram.v
+++ Pipelined_CPU/Async_FIFO/ram.v
@@ -1,18 +1,18 @@
-module ram(
-    input w_clk,                    //Write clock
-    input r_clk,                    //Read clock
-    input resetn,                   //Negative reset signal
-    input w_en,                     //Write enable
-    input r_en,                     //Read enable
-    input full,                     //Full condition
-    input empty,                    //Empty condition
-    input [10:0] w_adrs,            //Write address
-    input [10:0] r_adrs,            //Read address
-    input [31:0] w_data,            //Write data
-    output reg [31:0] r_data        //Read data
+module ram #(parameter mem_width=32, mem_depth = 8)(
+    input w_clk,                            //Write clock
+    input r_clk,                            //Read clock
+    input resetn,                           //Negative reset signal
+    input w_en,                             //Write enable
+    input r_en,                             //Read enable
+    input full,                             //Full condition
+    input empty,                            //Empty condition
+    input [mem_depth - 1:0] w_adrs,         //Write address
+    input [mem_depth - 1:0] r_adrs,         //Read address
+    input [mem_width:0] w_data,             //Write data
+    output reg [mem_width -1:0] r_data      //Read data
 );
 
-    reg [31:0] mem [2047:0];        //Memory
+    reg [mem_width - 1:0] mem [mem_depth - 1:0];        //Memory
     
     always @(posedge w_clk) begin: WRITE_TO_MEM
         if(!full && w_en && resetn) begin
