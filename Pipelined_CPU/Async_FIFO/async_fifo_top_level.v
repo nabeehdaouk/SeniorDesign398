e96cd29742a7662f376e2659b420aed5ce620f40
Sam Rojanasakdakul <sxr910@case.edu>
2023-10-09 17:27:18 -0400

    Update async_fifo_top_level.v

--- Pipelined_CPU/Async_FIFO/async_fifo_top_level.v
+++ Pipelined_CPU/Async_FIFO/async_fifo_top_level.v
@@ -2,6 +2,8 @@ module async_fifo_top_level #(parameter DATA_WIDTH = 31, MEM_DEPTH=8, PTR_SIZE=8
 	input w_clk,
 	input r_clk,
 	input resetn,
+	input r_en,
+	input w_en,
 	input [DATA_WIDTH-1:0] w_data,
 	input [MEM_DEPTH-1:0] r_adrs,
 	input [MEM_DEPTH-1:0] w_adrs,
@@ -10,30 +12,55 @@ module async_fifo_top_level #(parameter DATA_WIDTH = 31, MEM_DEPTH=8, PTR_SIZE=8
 	output reg empty
 	
 );
-	wire [PTR_SIZE - 1: 0] r_ptr;
+	wire [PTR_SIZE - 1: 0] rptr;
 	wire [PTR_SIZE - 1: 0] gray_rptr;
 	wire [PTR_SIZE - 1: 0] gray_rptr_sync;
 	wire [PTR_SIZE - 1: 0] rptr_sync;
-	wire [PTR_SIZE - 1: 0] w_ptr;
+	wire [PTR_SIZE - 1: 0] wptr;
 	wire [PTR_SIZE - 1: 0] gray_wptr;
 	wire [PTR_SIZE - 1: 0] gray_wptr_sync;
 	wire [PTR_SIZE - 1: 0] wptr_sync;
+	wire [PTR_SIZE - 1: 0] check_next;
+	
+	wire write;
+	wire read;
 	
 	
-	binary_to_gray binary_to_gray_instance(
-        .bin(r_ptr),
+	binary_to_gray #(.WIDTH(PTR_SIZE)) binary_to_gray_rptr(
+        .bin(rptr),
         .gray(gray_rptr)
     );
     
-	gray_to_binary gray_to_binary_instance(
-	    .bin(w_ptr),
+	binary_to_gray #(.WIDTH(PTR_SIZE)) binary_to_gray_wptr(
+	    .bin(wptr),
 	    .gray(gray_wptr)
 	);
 	
-	ram #(
-	    .MEM_WIDTH(DATA_WIDTH),
-	    .MEM_DEPTH(MEM_DEPTH)
-	) ram_instance(
+	gray_to_binary #(.WIDTH(PTR_SIZE)) gray_to_binary_rptr(
+	    .gray(gray_rptr_sync),
+	    .bin(rptr_sync)
+	);
+	
+	gray_to_binary #(.WIDTH(PTR_SIZE)) gray_to_binary_wptr(
+        .gray(gray_wptr_sync),
+        .bin(wptr_sync)
+    );
+    
+    ff_sync #(.DATA_WIDTH(PTR_SIZE)) ff_sync_read_to_write(
+        .clk(r_clk),
+        .resetn(resetn),
+        .data_in(gray_rptr),
+        .data_out(gray_rptr_sync)
+    );
+    
+    ff_sync #(.DATA_WIDTH(PTR_SIZE)) ff_sync_write_to_read(
+        .clk(w_clk),
+        .resetn(resetn),
+        .data_in(gray_wptr),
+        .data_out(gray_wptr_sync)
+    );
+    
+	ram #(.MEM_WIDTH(DATA_WIDTH),.MEM_DEPTH(MEM_DEPTH)) ram_instance(
 	    .w_clk(w_clk),
 	    .r_clk(r_clk),
 	    .resetn(resetn),
