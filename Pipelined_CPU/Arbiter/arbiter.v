743e1a80ba91d278d6c3319974c863ea0d49a8d9
nabeehdaouk <144377506+nabeehdaouk@users.noreply.github.com>
2023-10-29 16:55:56 -0400

    arb

--- Pipelined_CPU/Arbiter/arbiter.v
+++ Pipelined_CPU/Arbiter/arbiter.v
@@ -1,6 +1,14 @@
 module arbiter(
-	input clk,
-	input rst
+    input clk,
+	input [31:0] instruction,
+	output reg [31:0] FIFO_1, FIFO_2
 );
+	reg fifo_select;
 	
-endmodule : arbiter
+	always @(posedge clk)
+	begin
+	    fifo_select <= ~fifo_select;
+	    FIFO_1<= (fifo_select==1'b0)? instruction: FIFO_1;
+	    FIFO_2<= (fifo_select==1'b1)? instruction: FIFO_2;
+	end
+endmodule
